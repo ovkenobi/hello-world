0714337b8e34163f20a5e6a61bd926806f252b83
ovkenobi <ovkenobi@yandex.ru>
2016-05-22 19:11:13 +0500

    многое нужно доделать

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tmp
-import json
+from json import dumps
+from datetime import datetime
 from proxies import gen_proxy_handler
 from auto_e1_parse2 import all_href_car
 from auto_e1_parse2 import get_public_info
+from auto_e1_parse2 import get_private_info
 
 def gen_url(n):
     yield "http://auto.e1.ru/car/foreign/?region[]=213"
@@ -11,13 +13,33 @@ def gen_url(n):
 
 
 def main2():
+    url = "http://auto.e1.ru/offer/json"
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Content-Length': 0,
+        'Referer': url,
+        'Cookie': ""
+        }
+
     file = open("data.txt", 'w')
     count = 0
     handlers = gen_proxy_handler()
     for page_lavel_1 in tmp.load_page(gen_url(10), handlers):
-        for page_lavel_2 in tmp.load_page(all_href_car(page_lavel_1), handlers):
+        for page_lavel_2, head in tmp.load_page(all_href_car(page_lavel_1), handlers, True):
             count +=1
             d=get_public_info(page_lavel_2)
+            
+            data = dumps({"jsonrpc":"2.0",
+                          "method":"getOfferContacts",
+                          "params":[{"id":d['id'],
+                          "context":"card"}],
+                          "id":int(datetime.now().timestamp()*1000)}).encode()
+            headers['Content-Length'] = len(data)
+            headers['Cookie'] = head['Set-Cookie']
+            r=tmp.request_post(url, next(handlers), data, list(headers.items()))
+            if r:
+                d1=get_private_info(tmp.request_post())
+                if d1: d.update(d1)
             print(count, d['title'])
             file.write(json.dumps(d))
             file.write('\n')
--- tmp.py
+++ tmp.py
@@ -1,6 +1,9 @@
 from urllib import request
+from datetime import datetime
+from json import dumps
+from json import loads
 
-def load_page(iter_links, iter_proxy):
+def load_page(iter_links, iter_proxy, need_head=False):
     print('begin load_page')
     last_ok = True
     while True:
@@ -23,16 +26,68 @@ def load_page(iter_links, iter_proxy):
             if not text: break
             print('load link ok')
             last_ok = True
-            yield text
+            if need_head: yield text, dict(r.headers)
+            else: yield text
 
-#asdas			
-def request_post(url, proxy, body, headers=None):
-	if headers is None: headers={}
-	headers['Content-Length'] = len(body)
-	opener = request.build_opener(proxy)
-	opener.addheaders=headers
-	# method
-	# set body
-	try:
-		r = opener.open(url, timeout=30)
-	
+#asdas            
+def request_post(url, proxy, body=None, headers=None):
+    opener = request.build_opener(proxy)
+    opener.addheaders = headers
+    try:
+        r = opener.open(url, data=body, timeout=30)
+    except Exception as e:
+        print('Error =', e)
+        r = None
+    if r:
+#        print(dict(r.headers))
+        t=r.readall()
+        print(t)
+        t=t.decode()
+        t=loads(t)
+        print(t)
+        return t
+    print('None')
+    return None
+        
+if __name__ == "__main__":
+    print()
+    print()
+    print()
+    print()
+    print()
+    proxy_handler=request.ProxyHandler({'http':'http://89.235.174.160:8080'})
+    url = "http://auto.e1.ru/offer/json"
+    data = dumps({"jsonrpc":"2.0",
+              "method":"getOfferContacts",
+              "params":[{"id":"7356291",
+                         "context":"card"}],
+              "id":int(datetime.now().timestamp()*1000)})
+
+    data=data.encode()
+    
+    headers = [
+    ('Accept', 'application/json, text/javascript, */*; q=0.01'),
+    ('Content-Length', len(data)),
+    ('Referer', url),
+    ('Cookie', 'ngs_ttq=u:7ab7b9cd7017c3a1b331f8030bf1bf55;\
+                ngs_uid=w127Clcq97gyuAyVA8NYAg==; \
+                isMobile=false; \
+                _ym_uid=1462433727342287192; \
+                _ym_visorc_24038521=w; \
+                _ym_isad=2; \
+                __utma=8453157.762694507.1462433727.1462433727.1462433727.1; \
+                __utmb=8453157.6.10.1462433727; \
+                __utmc=8453157; \
+                __utmz=8453157.1462433727.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); \
+                ngs_avc=9; \
+                RT=; \
+                doh=10; \
+                presentationListHide=true; \
+                __utmt=1')
+    ]
+    request_post(url, proxy_handler, body=data, headers=headers)
+
+
+
+
+
