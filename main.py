af15c45de36b979b3969d763677b94c514a9f7d3
ovkenobi <ovkenobi@yandex.ru>
2016-05-24 16:25:23 +0500

    Худо бедно заработало Теперь надо все засунуть в класс и добавить парсинг Имени

--- main.py
+++ main.py
@@ -5,21 +5,42 @@ from proxies import gen_proxy_handler
 from auto_e1_parse2 import all_href_car
 from auto_e1_parse2 import get_public_info
 from auto_e1_parse2 import get_private_info
+from string import printable
+from random import sample
+from random import randrange
+
+
+def get_random_str(length):
+    return ''.join(sample(printable[:62],length))
 
 def gen_url(n):
     yield "http://auto.e1.ru/car/foreign/?region[]=213"
     for i in range(n-1):
         yield "http://auto.e1.ru/car/foreign/?region[]=213&page={}".format(i+2)
 
+def make_json_body(id_):
+    return dumps({"jsonrpc":"2.0",
+                  "method":"getOfferContacts",
+                  "params":[{"id": id_,
+                  "context":"card"}],
+                  "id":int(datetime.now().timestamp()*1000)}).encode()
+
+def make_headers(length, cookie_part):
+    return list({'Accept': 'application/json, text/javascript, */*; q=0.01',
+                 'Content-Length': length,
+                 'Referer': "http://auto.e1.ru/offer/json",
+                 'Cookie': cookie_part+"; ngs_ttq={}; ngs_uid={}; ngs_avc={};".format(get_random_str(34),get_random_str(24),randrange(7,40))
+                 }.items())
+
 
 def main2():
     url = "http://auto.e1.ru/offer/json"
-    headers = {
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Content-Length': 0,
-        'Referer': url,
-        'Cookie': ""
-        }
+#    headers = {
+#        'Accept': 'application/json, text/javascript, */*; q=0.01',
+#        'Content-Length': 0,
+#        'Referer': url,
+#        'Cookie': ""
+#        }
 
     file = open("data.txt", 'w')
     count = 0
@@ -27,24 +48,35 @@ def main2():
     for page_lavel_1 in tmp.load_page(gen_url(10), handlers):
         for page_lavel_2, head in tmp.load_page(all_href_car(page_lavel_1), handlers, True):
             count +=1
+#            cookies=head['Set-Cookie']+"; ngs_ttq={}; ngs_uid={}; ngs_avc={};".format(get_random_str(34),get_random_str(24),count%30+7)
+#            print(count, cookies)
             d=get_public_info(page_lavel_2)
-            
-            data = dumps({"jsonrpc":"2.0",
-                          "method":"getOfferContacts",
-                          "params":[{"id":d['id'],
-                          "context":"card"}],
-                          "id":int(datetime.now().timestamp()*1000)}).encode()
-            headers['Content-Length'] = len(data)
-            headers['Cookie'] = head['Set-Cookie']
-            r=tmp.request_post(url, next(handlers), data, list(headers.items()))
-            if r:
-                d1=get_private_info(tmp.request_post())
-                if d1: d.update(d1)
-            print(count, d['title'])
-            file.write(json.dumps(d))
-            file.write('\n')
-            file.flush()
+            data = make_json_body(d['id'])
+            handler = next(handlers)
+            for _ in range(3):
+                r = tmp.request_post(url, handler, data, make_headers(len(data), head['Set-Cookie']))
+                if r:
+                    d1=get_private_info(r)
+                    if d1: d.update(d1)
+                    break
+            print(count, d['title'], d.get('phone', ''))
+#            file.write(dumps(d))
+#            file.write('\n')
+#            file.flush()
     file.close()
+
+
+class ParserE1():
+    def __init__(self):
+        self.url = "http://auto.e1.ru/offer/json"
+        self.cur_proxy_handler=None
+        self.gen_proxy_handler=None
+        
+    def next_proxy_handler():
+        if self.gen_proxy_handler is None: raise Exception("Generator for proxy handlers is 'None'")
+        self.cur_proxy_handler = next(self.gen_proxy_handler)
+
+    
 
 
 if __name__ == "__main__":
--- tmp.py
+++ tmp.py
@@ -1,10 +1,11 @@
 from urllib import request
-from datetime import datetime
+from urllib import error
 from json import dumps
 from json import loads
+import http
 
 def load_page(iter_links, iter_proxy, need_head=False):
-    print('begin load_page')
+    print('load_page - start')
     last_ok = True
     while True:
         print("select proxy")
@@ -12,6 +13,7 @@ def load_page(iter_links, iter_proxy, need_head=False):
         print("proxy selected", proxy_handler.proxies['http'])
         opener = request.build_opener(proxy_handler)
         opener.addheaders.clear()
+        err_num=0
         while True:
             if last_ok: link = next(iter_links)
             print('link', link)
@@ -20,74 +22,73 @@ def load_page(iter_links, iter_proxy, need_head=False):
             try:
                 r = opener.open(link, timeout=30)
                 text = r.readall().decode()
-            except: 
-                print("error")
-                break
-            if not text: break
-            print('load link ok')
+            except http.client.HTTPException as e: print("HTTPException =", e)
+            except error.HTTPError as e: print('HTTPError =', e)
+            except error.URLError as e: print("URLError =", e)
+            if text is None:
+                err_num +=1
+                if err_num > 3: break
+                continue
+            print('load link - ok')
             last_ok = True
             if need_head: yield text, dict(r.headers)
             else: yield text
 
-#asdas            
 def request_post(url, proxy, body=None, headers=None):
     opener = request.build_opener(proxy)
     opener.addheaders = headers
+    print("request_post start")
+    t = None
     try:
         r = opener.open(url, data=body, timeout=30)
-    except Exception as e:
-        print('Error =', e)
-        r = None
-    if r:
-#        print(dict(r.headers))
-        t=r.readall()
-        print(t)
+        t = r.readall()
+    except http.client.HTTPException as e: print("HTTPException =", e)
+    except error.HTTPError as e: print('HTTPError =', e)
+    except error.URLError as e: print('URLError =', e)
+    except Exception as e: print("Exception =", e)
+    if t:
+        print("request_post Ok")
         t=t.decode()
-        t=loads(t)
-        print(t)
-        return t
+        #print(t)
+        return loads(t)['result']
     print('None')
     return None
-        
-if __name__ == "__main__":
-    print()
-    print()
-    print()
-    print()
-    print()
-    proxy_handler=request.ProxyHandler({'http':'http://89.235.174.160:8080'})
+
+def test_request_post():
+    from auto_e1_parse2 import get_private_info
+    proxy_handler=request.ProxyHandler({'http':'http://210.96.153.20:3128'})
     url = "http://auto.e1.ru/offer/json"
     data = dumps({"jsonrpc":"2.0",
               "method":"getOfferContacts",
-              "params":[{"id":"7356291",
+              "params":[{"id":"7684741",
                          "context":"card"}],
-              "id":int(datetime.now().timestamp()*1000)})
-
+              "id":1463980588546})
     data=data.encode()
-    
     headers = [
     ('Accept', 'application/json, text/javascript, */*; q=0.01'),
     ('Content-Length', len(data)),
     ('Referer', url),
-    ('Cookie', 'ngs_ttq=u:7ab7b9cd7017c3a1b331f8030bf1bf55;\
-                ngs_uid=w127Clcq97gyuAyVA8NYAg==; \
+    ('Cookie', 'ngs_ttq=u:7ab7b9cd7017c3a1b331a8030bf1bf;\
+                ngs_uid=w127Clcq97GyuAyVA8NYAg; \
                 isMobile=false; \
-                _ym_uid=1462433727342287192; \
-                _ym_visorc_24038521=w; \
-                _ym_isad=2; \
-                __utma=8453157.762694507.1462433727.1462433727.1462433727.1; \
-                __utmb=8453157.6.10.1462433727; \
-                __utmc=8453157; \
-                __utmz=8453157.1462433727.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); \
-                ngs_avc=9; \
-                RT=; \
-                doh=10; \
-                presentationListHide=true; \
-                __utmt=1')
+                ngs_avc=39; \
+                ')
     ]
-    request_post(url, proxy_handler, body=data, headers=headers)
+    res= request_post(url, proxy_handler, body=data, headers=headers)
+    print("type res = ", type(res))
+    print(get_private_info(res))
 
+def test_load_page():
+    url_list = ["http://auto.e1.ru/car/foreign/?region[]=213",
+                "http://auto.e1.ru/car/foreign/?region[]=213&page=2",
+                "http://auto.e1.ru/car/foreign/?region[]=213&page=3"]
+    proxy_list = [request.ProxyHandler({'http':'http://203.201.37.77:80'}),
+                  request.ProxyHandler({'http':'http://177.223.63.30:8080'}),
+                  request.ProxyHandler({'http':'http://210.96.153.20:3128'}),
+                  request.ProxyHandler({'http':'http://81.218.197.25:8088'})]
+    for text in load_page(iter(url_list), iter(proxy_list)):
+        print(text)
 
-
-
-
+if __name__ == '__main__':
+    #test_load_page()
+    test_request_post()
