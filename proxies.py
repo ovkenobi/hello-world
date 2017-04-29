7f7405161eafa1aa819c3ba13241e82ba57192ef
ovkenobi <ovkenobi@yandex.ru>
2017-04-29 10:17:08 +0500

    Поправил после парсинг сайта после его замены

--- proxies.py
+++ proxies.py
@@ -1,13 +1,18 @@
 from urllib import request
 import re
 
-def proxy_list(pages=1):
+def proxy_list(pages=1, loop = False):
     '''
-    Функция генератор, возвращает список доступных прокси с сайта samair.ru.
+    Функция генератор, возвращает список доступных прокси с сайта
+    samair.ru. - устарел, новый https://premproxy.com/proxy/
     page - кол-во страниц (от 1 до примерно 50) для поиска.
     На 1 странице 35 прокси.
     '''
-    def search(pattern, text):
+    def search_one(pattern, text):
+        '''
+        Функция поиска текста по шаблону
+        Ищет только одно совпадение
+        '''
         if pattern and text:
             match = re.search(pattern, text, re.DOTALL)
             if match:
@@ -15,24 +20,39 @@ def proxy_list(pages=1):
                 return text[i:j]
         return None
 
+    def search_all(pattern, text):
+        '''
+        Функция поиска текста по шаблону
+        Ищет все совпадения, возвращает лист
+        '''
+        if pattern and text:
+            return re.findall(pattern, text, re.DOTALL)
+        return None
+                
+
     def get_url(n=1):
-        if n==1: url = "http://samair.ru/proxy"
-        else: url = "http://samair.ru/proxy/proxy-%02d.htm" % (n)
-        try: t = request.urlopen(url).readall().decode()
-        except: t=None
-        t = search(r"Can\'t copy proxy ports\?.*?You can do it there", t)
-        t = search("href=.+?>", t)
-        if t: t="http://samair.ru"+t[6:-2]
-        return t
-    
-    for i in range(pages):
-        try: t = request.urlopen(get_url(i+1)).readall().decode()
-        except: t = None
-        t = search(r'id="content.+?</div>', t)
-        t = search(r"<pre>.+?</pre>", t)
-        if t:
-            for proxy in t[5:-6].strip().split('\n'):
-                yield tuple(proxy.split(':'))
+        url = "https://premproxy.com/proxy"
+        if n>1: url+= "/proxy-%02d.htm" % (n)
+        return url
+#        try: t = request.urlopen(url).readall().decode()
+#        except:
+#            t=None
+#        t = search(r'id="navbuttons', t)
+#        t = search("href=.+?>", t)
+#        if t: t="https://premproxy.com"+t[6:-2]
+#        return t
+
+    while True:
+        for i in range(pages):
+            try: t = request.urlopen(get_url(i+1)).readall().decode()
+            except: t = None
+            t = search_one(r'<table[^>]+?id="proxylist.+?</table>', t)
+            if t:
+                for proxy in search_all(r"<tr.+?<td>.+?</td>", t):
+                    proxy = search_one(r"<td>.+?</td>", proxy)
+                    if proxy:
+                        yield tuple(proxy[4:-5].strip().split(':'))
+        if not loop: break
 
 def check_proxy(ip,port):
     '''
@@ -84,7 +104,7 @@ def check_proxy3(ip, port):
         if ip not in t: return False
         return True
 
-def gen_proxy_handler(proxies = proxy_list(50)):
+def gen_proxy_handler(proxies = proxy_list(20)):
     '''
     Генератор получающий на входе список прокси, которые нужно проверить.
     На выход выдает список ProxyHandler, с проверенным прокси
@@ -92,6 +112,20 @@ def gen_proxy_handler(proxies = proxy_list(50)):
     for ip, port in proxies:
         if check_proxy2(ip, port):
             yield request.ProxyHandler({'http': 'http://{}:{}'.format(ip, port)})
+
+class ProxyGenerator():
+    def __init__(self):
+        self.cur_proxy_handler=None
+        self.proxy_gen=gen_proxy_handler(proxy_list(50, True))
+
+    def next_handler(self):
+        self.cur_proxy_handler = next(self.proxy_gen)
+        return self.cur_proxy_handler
+        
+    def get_handler(self):
+        if self.cur_proxy_handler is None: self.next_handler()
+        return self.cur_proxy_handler
+
 
 __proxies = (
     ("31.168.236.236",8080),
@@ -118,7 +152,7 @@ def test_gen_proxy_handler():
         print(handler.proxies['http'])
     print()
     print("test proxy gen 2")
-    for handler in gen_proxy_handler():
+    for handler in gen_proxy_handler(proxy_list()):
         print(handler.proxies['http'])
     print()
     print("--- end -------")
@@ -146,8 +180,24 @@ def test_proxy_list():
     print()
     print("--- end -------")
 
+def test_class_proxy_gen():
+    print("--- testing ---")
+    print("test class gen - 1")
+    gen = ProxyGenerator()
+    for _ in range(3):
+        print(gen.get_handler().proxies['http'])
+    print("test class gen - 2")
+    for _ in range(3):
+        print(gen.next_handler().proxies['http'])
+    print("test class gen - 3")
+    for _ in range(3):
+        print(gen.next_handler().proxies['http'])
+        print(gen.get_handler().proxies['http'])
+    print("--- end -------")
+
 def main():
-    test_gen_proxy_handler();
+    test_class_proxy_gen()
+#    test_gen_proxy_handler()
 #    test_proxy_list()
 #    test_check_proxy_function(check_proxy)
 #    test_check_proxy_function(check_proxy2)
